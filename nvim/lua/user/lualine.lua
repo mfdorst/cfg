98219c9160f76647d581a0106b29c39c65cef671
Michael Dorst <m@mdorst.net>
2022-07-27 11:53:34 -0700

    Update lualine config, disable airline

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -63,11 +63,12 @@ local spaces = function()
 end
 
 lualine.setup({
+    -- Powerline symbols: 
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
