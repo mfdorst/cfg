051ddfc355bcd38cc502107fa95ef71bde7b4d56
Michael Dorst <m@mdorst.net>
2022-08-13 00:57:54 -0700

    Add abbreviations to fish config

--- config.fish
+++ config.fish
@@ -27,17 +27,52 @@ if status is-interactive
     abbr -ag gl git log --oneline --graph
     abbr -ag gla git log --oneline --graph --branches --remotes --tags HEAD
     abbr -ag gll git log --graph
+    abbr -ag gb git branch
+    abbr -ag gr git reset
+    abbr -ag grh git reset --hard
+    abbr -ag grhh git reset --hard HEAD
+    abbr -ag grah "git add -A && git reset --hard HEAD"
+
+    ## Checkout next commit in the direction of the argument
+    ## Note: only capable of moving forward in history, not backward
+    ## Ex: `gmt master` would move one commit towards master
+    function gmt
+        if test (count $argv) != 1
+            echo "Usage: gmt <commit|branch|tag>"
+            return
+        end
+        git checkout $(git rev-list --topo-order HEAD..$argv | tail -1)
+    end
+
+    ## Exa
+    if command -v exa &> /dev/null
+        abbr -ag ls exa --git -l
+        abbr -ag lsa exa --git -al
+        abbr -ag lst exa --git -lTL2
+        abbr -ag lsta exa --git -laTL2
+    else
+        abbr -ag ls ls -l
+        abbr -ag lsa ls -la
+    end
 
     ## Other
     abbr -ag cls "clear; printf '\e[3J'"
 
+    ## Fluvio development
+    alias flub 'cargo build --manifest-path="$HOME/code/fluvio/Cargo.toml" --bin fluvio-run && cargo build --manifest-path="$HOME/code/fluvio/Cargo.toml" --bin fluvio'
+    alias flu "$HOME/code/fluvio/target/debug/fluvio"
+    abbr -ag flubr 'flub && flu'
+    abbr -ag flud 'flub && flu cluster start --local --develop'
+
     # Environment Variables
     set -x EDITOR nvim
+    set -x DOCKER_HOST unix:///var/run/docker.sock
 
     # Zoxide
     zoxide init fish --cmd cd | source
     
-    # Add cargo installs to path
     fish_add_path $HOME/.cargo/bin
+    fish_add_path $HOME/.fluvio/bin
+    fish_add_path $HOME/scripts
 end
 
